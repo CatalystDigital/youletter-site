ac77e5733706d68c63971c850ec49ab533ca6bd6
XminifridgeX <rhails777@gmail.com>
2023-02-20 13:53:29 -0500

    remove aws exports since in gitignore

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ import reportWebVitals from "./reportWebVitals";
 // aplify imports
 import { AmplifyProvider } from "@aws-amplify/ui-react"; //remove
 import { Amplify } from "aws-amplify";
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //rmove this bitch as well
-  <AmplifyProvider> 
+  <AmplifyProvider>
     <App />
   </AmplifyProvider>
 );
